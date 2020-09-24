cdbf9d9e5d08683c4341c8bc60a8af5356df1004
DiSpace <cleverclub76@gmail.com>
2020-09-24 16:55:22 +0300

    добавлен конфиг для api озона

---
+++ config/ozone.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'client_id' => env('OZONE_CLIENT_ID', '836'),
+    'api_key' => env('OZONE_API_KEY', '0296d4f2-70a1-4c09-b507-904fd05567b9'),
+    'host' => env('OZONE_HOST', 'http://cb-api.ozonru.me/'),
+];
