d4e3e155c19a6bd9564f271f494b317662374acd
DiSpace <cleverclub76@gmail.com>
2020-09-24 16:46:39 +0300

    добавлен swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,17 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Swagger(
+ *   schemes={"http"},
+ *   host="localhost",
+ *   basePath="/api/",
+ *   @OA\Info(
+ *     title="Ozone Test API",
+ *     version="1.0.0"
+ *   )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
