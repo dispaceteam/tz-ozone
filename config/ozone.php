8e00441eadee8eec902acc51f80a011e761e42da
DiSpace <cleverclub76@gmail.com>
2020-09-28 02:09:30 +0300

    в конфиг добавлены статусы

--- config/ozone.php
+++ config/ozone.php
@@ -5,4 +5,12 @@ return [
     'client_id' => env('OZONE_CLIENT_ID', '836'),
     'api_key' => env('OZONE_API_KEY', '0296d4f2-70a1-4c09-b507-904fd05567b9'),
     'host' => env('OZONE_HOST', 'http://cb-api.ozonru.me/'),
+
+    'products' => [
+        'status' => [
+            'failed' => ['failed_moderation', 'failed_validation', 'failed'],
+            'in_progress' => ['sent', 'processing', 'moderating'],
+            'success' => ['processed']
+        ]
+    ]
 ];
